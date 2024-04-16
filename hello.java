20960e80dfaa402365487dc58afdeb66cac334d0
root <root@ip-172-31-4-15.us-east-2.compute.internal>
2024-04-16 01:55:37 +0000

    CICD-TEST

--- hello.java
+++ hello.java
@@ -2,7 +2,6 @@
 public class hello {
     public static void main(String []args)
     {
-        System.out.println("My First Java Program.");
         System.out.println("Welcome to the world of SDET!!!!!");
     }
 }
